5b5002129508b0f17fe6b3d32def25e5dd97c1a3
Realdo Dias <realdo@omesti.com>
2020-11-02 22:49:47 +0800

    updates to deploy to heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/real-users'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/dist/real-users/index.html'));
+});
+
+// start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
