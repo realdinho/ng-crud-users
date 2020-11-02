8717e0e83319648257369c8bdf8aba9c10c27c9b
Realdo Dias <realdo@omesti.com>
2020-11-02 18:55:09 +0800

    setup project and add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { DetailsUserComponent } from './components/details-user/details-user.component';
+import { ListUsersComponent } from './components/list-users/list-users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: 'users', component: ListUsersComponent },
+  { path: 'user/:id', component: DetailsUserComponent },
+  { path: 'add', component: AddUserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { ListUsersComponent } from './components/list-users/list-users.component';
+import { DetailsUserComponent } from './components/details-user/details-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent,
+    ListUsersComponent,
+    DetailsUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
